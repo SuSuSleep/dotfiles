c9941724eb34c961e1843695e15d767b970ad88c
SuSuSleep <joh449449@gmail.com>
2024-03-03 12:41:00 +0800

    Update

--- wezterm/.config/wezterm.lua
+++ wezterm/.config/wezterm.lua
@@ -14,7 +14,13 @@ end
 
 -- For example, changing the color scheme:
 config.color_scheme = "Gruvbox Material (Gogh)"
-config.default_domain = "WSL:Ubuntu"
+config.default_domain = "WSL:Ubuntu-22.04"
+config.window_padding = {
+	left = "1cell",
+	right = "1cell",
+	top = "0cell",
+	bottom = "0cell",
+}
 
 -- and finally, return the configuration to wezterm
 return config
